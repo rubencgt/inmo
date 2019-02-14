14ec750b1567cbb783fb6ef0cc02fbe3df8bbc33
Ruben <rubencgt@gmail.com>
2019-02-14 08:33:34 +0100

    comit

--- src/test/java/com/sistema/inmobiliaria/inmo/steps/PersonSteps.java
+++ src/test/java/com/sistema/inmobiliaria/inmo/steps/PersonSteps.java
@@ -1,46 +1,30 @@
-package com.sistema.inmobiliaria.inmo.steps;
+package com.sistema.inmobiliaria.inmo.Steps;
 
-import com.sistema.inmobiliaria.inmo.domain.user.User;
-import com.sistema.inmobiliaria.inmo.domain.user.UserDto;
-import com.sistema.inmobiliaria.inmo.domain.user.UserRepository;
-import com.sistema.inmobiliaria.inmo.rest.RegistrationController;
+import com.sistema.inmobiliaria.inmo.domain.user.Person;
+import com.sistema.inmobiliaria.inmo.domain.user.PersonRepository;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
-import org.json.JSONException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 
-import java.util.Objects;
+import static com.sistema.inmobiliaria.inmo.utils.DefaultEntities.unique_email;
 
 public class PersonSteps extends StepsRestBaseIT {
 
     @Autowired
-    private UserRepository userRepository;
+    private PersonRepository personRepository;
 
-    @Given("a new person is registered")
+    @Given("a new person")
     public void personIsCreated() {
-        UserDto userDto = createUserDto();
-        restTemplate.postForEntity(inmoUrlBase + RegistrationController.BASE_URL, userDto, UserDto.class);
-        cucumberContext.setTestUser(userDto);
-    }
-
-    @Then("the person can access authorized resources")
-    public void personAccessPrivateResources() throws JSONException {
-        HttpEntity<?> entity = new HttpEntity<>("parameters", getHeadersWithToken(cucumberContext.getTestUser().getEmail(), cucumberContext.getTestUser().getPassword()));
-        ResponseEntity<String> responseEntity2 = restTemplate.exchange(inmoUrlBase + "/user", HttpMethod.GET, entity, String.class);
-        assert responseEntity2.getStatusCode().is2xxSuccessful();
-        assert Objects.equals(responseEntity2.getBody(), "Logged in from /user");
+        Person person = new Person("some name", "name", unique_email());
+        personRepository.save(person);
+        cucumberContext.setCustomerId(person.getId());
     }
 
 
     @Then("the person audit fields are filled")
     public void personAuditFieldsAreFilled() {
-        User user = userRepository.findUserByEmail(cucumberContext.getTestUser().getEmail()).get();
-        assert user.getCreationDate() != null;
-        assert user.getCreatedBy() != null;
-        assert user.getLastModifiedBy() == null;
-        assert user.getLastModifiedDate() == null;
+        Person person = personRepository.findById(cucumberContext.getCustomerId());
+        assert person.getCreationDate() != null;
+        assert person.getCreatedBy() != null;
     }
 }
